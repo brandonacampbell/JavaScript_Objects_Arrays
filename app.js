7e35e4100656dbb976f3cfeda01290fcb415c6aa
Brandon Campbell <abrandoncampbell@gmail.com>
2023-01-26 12:38:06 -0500

    completed all Exercises

--- app.js
+++ app.js
@@ -1,9 +1,75 @@
-console.log("Hello World!\n==========\n");
+// Exercise 1
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+let sum = 0;
 
-// Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+const numbers = [2, 22, 12, 17, 18, 39, 129];
 
 
+function arraySum(param){
+param.forEach(element => {
+    sum += element;
+});
+return sum;
+}
+
+console.log(arraySum(numbers));
+
+//Exercise 2
+
+let book = {
+    title: 'To Kill a Mockingbird' ,
+    author: 'Harper Lee', 
+    pages: 342, 
+    read: true,
+    readCount: 2, 
+};
+
+book.info = function(){
+    return `${book.title} by ${book.author},  ${book.pages} pages, read ${book.readCount} times.`
+};
+
+console.log(book.info())
+
+//Exercise 3
+
+let testSentence = "The quick brown fox jumps over the lazy dog";
+function reverseWords (sentence){
+    let arr = sentence.split(" ");
+    var result = [];
+    for (let word of arr) {
+        var chars = word.split('');
+        chars.reverse();
+        var reversedWord = chars.join('');
+        result.push(reversedWord);
+    }
+
+    return result.join(' ');
+}
+
+console.log(reverseWords(testSentence));
+
+// Exercise 4 
+let csvData = "name,age\nFrodo,50\nSam,38\nMerry,36\nPippin,26";
+
+function csvConverter(data){
+    let headers = data.slice(0, data.indexOf("\n")).split(",");
+
+    let valuesList = data.slice(data.indexOf("\n") + 1).split("\n");
+
+    const customArr = valuesList.map((row) => {
+        let values = row.split(",");
+        let obj = {};
+
+        values.forEach((col, idx) => {
+            if (idx < headers.length) {
+                obj[headers[idx]] = col;    
+            } else {
+                obj[`misc${idx}`] = col;
+            }
+        });
+        return obj;
+    });
+  return customArr;
+}
+
+console.log(csvConverter(csvData));
